a6c36457a0902101dabafeb47898b22f517846a6
Daryll Ko <daryll.ko@gmail.com>
2023-03-04 15:30:30 +0800

    feat: add description content

---
+++ src/routes/description.svelte
@@ -0,0 +1,10 @@
+<h1>Our Challenge</h1>
+<h2>Ideate.</h2>
+<h2>Innovate.</h2>
+<h2>Integrate.</h2>
+<p>
+	Identify a specific local problem and provide a relevant and effective solution based on the
+	general problem provided. Submissions include an Innovation Proposal, Working Prototype, and a
+	Live Presentation. In addition to the scope of the problem statement, be sure to address at least
+	1 of the 17 UN Sustainable Goals while using at least 1 Google Technology in your solution.
+</p>
