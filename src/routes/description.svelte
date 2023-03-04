f0a441cde662d01165bc145cd86156f88c32bfcf
Daryll Ko <daryll.ko@gmail.com>
2023-03-04 19:58:07 +0800

    feat: style description section

--- src/routes/description.svelte
+++ src/routes/description.svelte
@@ -1,12 +1,19 @@
-<h1 id="description">Our Challenge</h1>
-<h2>Ideate.</h2>
-<h2>Innovate.</h2>
-<h2>Integrate.</h2>
-<p>
-	Identify a specific local problem and provide a relevant and effective solution based on the
-	general problem provided. Submissions include an Innovation Proposal, Working Prototype, and a
-	Live Presentation. In addition to the scope of the problem statement, be sure to address at least
-	1 of the 17 UN Sustainable Goals while using at least 1 Google Technology in your solution.
-</p>
+<section class="py-20 px-8" id="description">
+	<h1 class="mb-10 text-center text-6xl font-bold">Our Challenge</h1>
+	<div class="flex flex-col items-center justify-center gap-10 lg:flex-row">
+		<div class="flex flex-col items-center gap-3 lg:items-start">
+			<h2 class="text-3xl text-red-500 drop-shadow-md">Ideate.</h2>
+			<h2 class="text-4xl text-yellow-500 drop-shadow-lg">Innovate.</h2>
+			<h2 class="text-5xl text-emerald-500 drop-shadow-xl">Integrate.</h2>
+		</div>
+		<p class="sm:max-w-[80%] lg:max-w-[60%] xl:max-w-[40%]">
+			Identify a specific local problem and provide a relevant and effective solution based on the
+			general problem provided. Submissions include an Innovation Proposal, Working Prototype, and a
+			Live Presentation. In addition to the scope of the problem statement, be sure to address at
+			least 1 of the 17 UN Sustainable Goals while using at least 1 Google technology in your
+			solution.
+		</p>
+	</div>
+</section>
 
 <style lang="postcss"></style>
