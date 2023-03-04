c86c246ed06a6aca8c6617d2a71548490f7abe07
Daryll Ko <daryll.ko@gmail.com>
2023-03-04 17:16:15 +0800

    feat: add Footer content

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,1 +1,8 @@
-<h2>Footer</h2>
+<footer>
+	<div>
+		<a href="https://www.facebook.com/DilimanSoCha">Facebook</a>
+		<a href="/">Discord</a>
+		<a href="https://gdscupd.com">Website</a>
+	</div>
+	<p>© DSC UP Diliman 2023. All Rights Reserved.</p>
+</footer>
