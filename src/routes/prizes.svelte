c3350979b032d40fa220ac9455a6329825315257
Daryll Ko <daryll.ko@gmail.com>
2023-03-04 16:59:08 +0800

    feat: add barebones Prizes section

--- src/routes/prizes.svelte
+++ src/routes/prizes.svelte
@@ -1,1 +1,11 @@
-<h2 id="prizes">Prizes</h2>
+<h1 id="prizes">Prizes</h1>
+<h2>PHP 20,000</h2>
+<p>GRAND WINNER</p>
+<h2>PHP 10,000</h2>
+<p>1ST RUNNER UP</p>
+<h2>PHP 5,000</h2>
+<p>2ND RUNNER UP</p>
+<h2>PHP 3,000</h2>
+<p>BEST INNOVATION PROPOSAL</p>
+<p>EXCLUSIVE EVENT FROM ACCENTURE</p>
+<p>CERTIFICATE OF PARTICIPATION</p>
