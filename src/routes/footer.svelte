4bc9251309a7384f310de3b3fc5135e655eb3970
Daryll Ko <daryll.ko@gmail.com>
2023-03-04 22:18:52 +0800

    fix: remove Discord from footer socials

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,6 +1,5 @@
 <script>
 	import FaFacebook from 'svelte-icons/fa/FaFacebook.svelte';
-	import FaDiscord from 'svelte-icons/fa/FaDiscord.svelte';
 	import FaGlobe from 'svelte-icons/fa/FaGlobe.svelte';
 </script>
 
@@ -9,7 +8,6 @@
 		<a href="https://www.facebook.com/DilimanSoCha" class="h-10 transition-all hover:text-[#1877F2]"
 			><FaFacebook /></a
 		>
-		<a href="/" class="h-10 transition-all hover:text-[#5865F2]"><FaDiscord /></a>
 		<a href="https://gdscupd.com" class="h-10 transition-all hover:text-[#4285F4]"><FaGlobe /></a>
 	</div>
 	<p class="text-center">Ⓒ DSC UP Diliman 2023. All Rights Reserved.</p>
