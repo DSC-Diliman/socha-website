46192fc4cdec7c8e6db467ccda8f02708d496d14
Daryll Ko <daryll.ko@gmail.com>
2023-03-04 21:06:37 +0800

    feat: style footer

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,10 +1,18 @@
-<footer>
-	<div>
-		<a href="https://www.facebook.com/DilimanSoCha">Facebook</a>
-		<a href="/">Discord</a>
-		<a href="https://gdscupd.com">Website</a>
+<script>
+	import FaFacebook from 'svelte-icons/fa/FaFacebook.svelte';
+	import FaDiscord from 'svelte-icons/fa/FaDiscord.svelte';
+	import FaGlobe from 'svelte-icons/fa/FaGlobe.svelte';
+</script>
+
+<footer class="py-10 px-8 lg:flex lg:items-center lg:justify-center lg:gap-10">
+	<div class="mb-5 flex items-center justify-center gap-5 lg:mb-0">
+		<a href="https://www.facebook.com/DilimanSoCha" class="h-10 transition-all hover:text-[#1877F2]"
+			><FaFacebook /></a
+		>
+		<a href="/" class="h-10 transition-all hover:text-[#5865F2]"><FaDiscord /></a>
+		<a href="https://gdscupd.com" class="h-10 transition-all hover:text-[#4285F4]"><FaGlobe /></a>
 	</div>
-	<p>© DSC UP Diliman 2023. All Rights Reserved.</p>
+	<p class="text-center">Ⓒ DSC UP Diliman 2023. All Rights Reserved.</p>
 </footer>
 
 <style lang="postcss"></style>
